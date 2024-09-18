b55a1c88e27e72a0ef77b444f0b3dabbba4731cf
TomasAyerbe <tomayerbe@gmail.com>
2024-09-18 17:50:08 -0300

    Add utils

---
+++ tests/utils/http.util.spec.ts
@@ -0,0 +1,80 @@
+import { get, post } from '../../src/utils/http.util';
+
+global.fetch = jest.fn();
+
+describe('http.util', () => {
+  beforeEach(() => {
+    (fetch as jest.Mock).mockClear();
+  });
+
+  describe('get', () => {
+    it('should return data when fetch is successful', async () => {
+      const mockResponse = { data: 'test' };
+
+      (fetch as jest.Mock).mockResolvedValue({
+        json: jest.fn().mockResolvedValue(mockResponse),
+      });
+
+      const result = await get('https://example.com');
+
+      expect(result).toEqual(mockResponse);
+
+      expect(fetch).toHaveBeenCalledWith('https://example.com', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      });
+    });
+
+    it('should return error and status when fetch fails', async () => {
+      const mockError = new Error('Fetch failed');
+
+      (fetch as jest.Mock).mockRejectedValue(mockError);
+
+      const result = await get('https://example.com');
+
+      expect(result).toEqual({
+        error: mockError,
+        status: 500,
+      });
+    });
+  });
+
+  describe('post', () => {
+    it('should return data when fetch is successful', async () => {
+      const mockResponse = { data: 'test' };
+
+      (fetch as jest.Mock).mockResolvedValue({
+        json: jest.fn().mockResolvedValue(mockResponse),
+      });
+
+      const result = await post('https://example.com', { key: 'value' });
+
+      expect(result).toEqual(mockResponse);
+
+      expect(fetch).toHaveBeenCalledWith('https://example.com', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+        body: JSON.stringify({ key: 'value' }),
+      });
+    });
+
+    it('should return error and status when fetch fails', async () => {
+      const mockError = new Error('Fetch failed');
+
+      (fetch as jest.Mock).mockRejectedValue(mockError);
+
+      const result = await post('https://example.com', { key: 'value' });
+
+      expect(result).toEqual({
+        error: mockError,
+        status: 500,
+      });
+    });
+  });
+});
